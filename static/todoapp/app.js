f710f809eac446421272f1beecbba1fbb07881e2
Luis Fernando Barrera <luisfernando@informind.com>
2014-10-15 23:23:49 -0500

    Todo Model Fixtures

--- static/todoapp/app.js
+++ static/todoapp/app.js
@@ -1,19 +1,2 @@
 window.Todos = Ember.Application.create({});
-
-Todos.Todo.FIXTURES = [
- {
-   id: 1,
-   title: 'Learn Ember.js',
-   isCompleted: true
- },
- {
-   id: 2,
-   title: '...',
-   isCompleted: false
- },
- {
-   id: 3,
-   title: 'Profit!',
-   isCompleted: false
- }
-];
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
