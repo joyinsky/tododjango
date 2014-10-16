5d6a5dc900d53dc9fac7a8f24a7b21e6139d0977
Luis Fernando Barrera <luisfernando@informind.com>
2014-10-16 01:09:21 -0500

    Todos Controller to create new instance

---
+++ static/todoapp/controllers/todos.js
@@ -0,0 +1,21 @@
+Todos.TodosController = Ember.ArrayController.extend({
+    actions: {
+        createTodo: function() {
+            var title = this.get('newTitle');
+            if (!title) {
+                return false;
+            }
+            if (!title.trim()) {
+                return undefined;
+            }
+
+            var todo = this.store.createRecord('todo', {
+                title: title,
+                isCompleted: false
+            });
+
+            this.set('newTitle',  '');
+            todo.save()
+        }
+    }
+});
