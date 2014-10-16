497b9295177ba103e6726ba88b6f12e045f69f10
Luis Fernando Barrera <luisfernando@informind.com>
2014-10-15 23:28:51 -0500

    Comment for ApplicationAdapter

--- static/todoapp/app.js
+++ static/todoapp/app.js
@@ -1,2 +1,5 @@
 window.Todos = Ember.Application.create({});
+
+// Adapters are responsible for communicating with a source of data for your application.
+// Typically this will be a web service API, but in this case we are using an adapter designed to load fixture data
 Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
