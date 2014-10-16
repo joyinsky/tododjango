2c6850203bae73db4de3669dd0fa17444d6fcf97
Luis Fernando Barrera <luisfernando@informind.com>
2014-10-16 02:56:55 -0500

    MARKING A MODEL AS COMPLETE OR INCOMPLETE DISPLAYING THE NUMBER OF INCOMPLETE TODOS

--- static/todoapp/controllers/todos.js
+++ static/todoapp/controllers/todos.js
@@ -17,5 +17,26 @@ Todos.TodosController = Ember.ArrayController.extend({
             this.set('newTitle',  '');
             todo.save()
         }
-    }
+    },
+    remaining: function () {
+        return this.filterBy('isCompleted', false).get('length');
+    }.property('@each.isCompleted'),
+    inflection: function() {
+        var remaining = this.get('remaining');
+        return remaining === 1 ? 'item': 'items';
+    }.property('remaining')
+});
+
+Todos.TodoController = Ember.ObjectController.extend({
+    isCompleted: function(key, value) {
+        var model = this.get('model');
+
+        if (value === undefined) {
+            return model.get('isCompleted');
+        } else {
+            model.set('isCompleted', value);
+            model.save();
+            return value;
+        }
+    }.property('model.isCompleted')
 });
