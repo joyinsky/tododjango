360a4805018661ffaf39fc3f7ebc22e02bc1123a
Luis Fernando Barrera <luisfernando@informind.com>
2014-10-16 04:16:52 -0500

    Filtering Transitions

--- static/todoapp/router.js
+++ static/todoapp/router.js
@@ -1,5 +1,8 @@
 Todos.Router.map(function() {
-   this.resource('todos', {path: '/'}, function() {});
+   this.resource('todos', {path: '/'}, function() {
+       this.route('active');
+       this.route('completed');
+   });
 });
 
 
@@ -12,5 +15,27 @@ Todos.TodosRoute = Ember.Route.extend({
 Todos.TodosIndexRoute = Ember.Route.extend({
     model: function() {
         return this.modelFor('todos');
+    }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+    model: function () {
+        return this.store.filter('todo', function(todo) {
+           return !todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function(controller) {
+        this.render('todos/index', {controller: controller});
+    }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+    model: function () {
+        return this.store.filter('todo', function(todo) {
+           return todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function(controller) {
+        this.render('todos/index', {controller: controller});
     }
 });
